dfd8bde5637ff52e755868b31433214f95728e88
Alex QAsper <semidog@gmail.com>
2022-07-03 14:48:57 +0300

    task 56 solved

--- Program.cs
+++ Program.cs
@@ -46,9 +46,27 @@ void Print(int[,] arr)
 /* Задача 56: Задайте прямоугольный двумерный массив. Напишите
 программу, которая будет находить строку с наименьшей суммой элементов. */
 
-int[,] mass = new int[5, 5];
+/* int[,] mass = new int[5, 5];
+int sum = int.MaxValue;
+int index = 0;
 FillArray(mass);
 Print(mass);
+
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    int temp = 0;
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        temp += mass[i, j];
+    }
+    if (temp < sum)
+    {
+        sum = temp;
+        index = i;
+    }
+}
+Console.WriteLine($"Строка: {index + 1}. Сумма: {sum}");
 
 int[,] FillArray(int[,] array)
 {
@@ -72,4 +90,4 @@ void Print(int[,] arr)
         }
         Console.WriteLine();
     }
-}
+} */
